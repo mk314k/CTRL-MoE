590a82c0397c1c716fb0b5e3291139d16602e74c
mk314k <mishrakesh3.14kartik@gmail.com>
2024-12-05 01:27:58 -0500

    contextual sumo env rewards

--- envs/contextual_sumo.py
+++ envs/contextual_sumo.py
@@ -1,6 +1,13 @@
 from sumo_rl import SumoEnvironment
 
 class SUMOContextualEnv(SumoEnvironment):
+    def change_context_reward_weight(self, pw, asw, dww, tqw):
+        #change with context
+        self.pressure_weight = 0
+        self.avg_speed_weight = 0
+        self.diff_wait_weight = 1
+        self.total_queue_weight = -1
+
     def _compute_rewards(self):
         self.rewards.update(
             {
@@ -12,18 +19,12 @@ class SUMOContextualEnv(SumoEnvironment):
         return {ts: self.rewards[ts] for ts in self.rewards.keys() if self.traffic_signals[ts].time_to_act or self.fixed_ts}
     
     def _process_ts_reward(self, ts):
-        #change with context
-        pressure_weight = 0
-        avg_speed_weight = 0
-        diff_wait_weight = 1
-        total_queue_weight = -1
-
-        reward = pressure_weight * self.traffic_signals[ts].get_pressure()
-        reward += avg_speed_weight * self.traffic_signals[ts].get_average_speed()
-        reward += total_queue_weight * self.traffic_signals[ts].get_total_queued()
-        if diff_wait_weight != 0:
+        reward = self.pressure_weight * self.traffic_signals[ts].get_pressure()
+        reward += self.avg_speed_weight * self.traffic_signals[ts].get_average_speed()
+        reward += self.total_queue_weight * self.traffic_signals[ts].get_total_queued()
+        if self.diff_wait_weight != 0:
             ts_wait = sum(self.traffic_signals[ts].get_accumulated_waiting_time_per_lane()) / 100.0
-            reward += diff_wait_weight * (self.traffic_signals[ts].last_measure - ts_wait)
+            reward += self.diff_wait_weight * (self.traffic_signals[ts].last_measure - ts_wait)
             self.traffic_signals[ts].last_measure = ts_wait
 
         self.traffic_signals[ts].last_reward = reward
